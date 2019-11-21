7d5454b0899c6e99db8b3fd25cbb88affd52c6f4
gtxbgt <37449669+gtxbgt@users.noreply.github.com>
2019-11-20 22:48:16 -0800

    Add files via upload

---
+++ gofood.php
@@ -0,0 +1,115 @@
+<?php
+
+error_reporting(0);
+if (!file_exists('token')) {
+    mkdir('token', 0777, true);
+}
+
+include ("curl.php");
+echo "\n";
+echo "\e[94m                   RePUBLIK GEDEL               \n";
+echo "\e[91m    RASAH CRIGIS RASAH CEREWET DINGGO DEWE WAE  \n";
+echo "\e[96m          GOFOOD SANTAI GOPULSA LAN GORIDE      \n";
+echo "\n";
+echo "\e[96m[?] 	TULISEN NOMER HENGPUNE NANG KENE    : ";
+$nope = trim(fgets(STDIN));
+$register = register($nope);
+if ($register == false)
+    {
+    echo "\e[91m[x] Nomor Wes Kanggo ganti liyane NYUK \n";
+    }
+  else
+    {
+    otp:
+    echo "\e[96m[!] KODE VERIFIKASI OTP NE TULIS NENG KENE : ";
+    $otp = trim(fgets(STDIN));
+    $verif = verif($otp, $register);
+    if ($verif == false)
+        {
+        echo "\e[91m[x] OTP MU SALAH NYUK \n";
+        goto otp;
+        }
+      else
+        {
+        file_put_contents("token/".$verif['data']['customer']['name'].".txt", $verif['data']['access_token']);
+        echo "\e[93m[!] Jajal Voucher : GOFOODSANTAI19 !\n";
+        sleep(3);
+        $claim = claim($verif);
+        if ($claim == false)
+            {
+            echo "\e[92m[!]".$voucher."\n";
+            sleep(3);
+            echo "\e[93m[!] Jajal Voucher: GOFOODSANTAI11 !\n";
+            sleep(3);
+            goto next;
+            }
+            else{
+                echo "\e[92m[+] ".$claim."\n";
+                sleep(3);
+                echo "\e[93m[!] Jajal Voucher : COBAINGOJEK !\n";
+                sleep(3);
+                goto ride;
+            }
+            next:
+            $claim = claim1($verif);
+            if ($claim == false) {
+                echo "\e[92m[!]".$claim['errors'][0]['message']."\n";
+                sleep(3);
+                echo "\e[93m[!] Jajal Voucher : GOFOODPASTA19 !\n";
+                sleep(3);
+                goto next1;
+            }
+            else{
+                echo "\e[92m[+] ".$claim."\n";
+                sleep(3);
+                echo "\e[93m[!] Jajal Voucher : COBAINGOJEK !\n";
+                sleep(3);
+                goto ride;
+            }
+            next1:
+            $claim = claim2($verif);
+            if ($claim == false) {
+                echo "\e[92m[!]".$claim['errors'][0]['message']."\n";
+                sleep(3);
+                echo "\e[93m[!] Jajal Voucher : COBAINGOJEK !\n";
+                sleep(3);
+                goto ride;
+            }
+          else
+            {
+            echo "\e[92m[+] ".$claim . "\n";
+            sleep(3);
+            echo "\e[93m[!] Jajal Voucher : COBAINGOJEK !\n";
+            sleep(3);
+            goto ride;
+            }
+            ride:
+            $claim = ride($verif);
+            if ($claim == false ) {
+                echo "\e[92m[!]".$claim['errors'][0]['message']."\n";
+                sleep(3);
+                echo "\e[93m[!] Trying to redeem Voucher : AYOCOBAGOJEK !\n";
+                sleep(3);
+
+            }
+            else{
+                echo "\e[92m[+] ".$claim."\n";
+                sleep(3);
+                echo "\e[93m[!] Trying to redeem Voucher : AYOCOBAGOJEK !\n";
+                sleep(3);
+                goto pengen;
+            }
+            pengen:
+            $claim = cekvocer($verif);
+            if ($claim == false ) {
+                echo "\033VOUCHER BOSOK/GAGAL LIK BALENI MENEH YO\n";
+            }
+            else{
+                echo "\e[92m[+] ".$claim."\n";
+                
+        }
+    }
+    }
+
+
+?>
